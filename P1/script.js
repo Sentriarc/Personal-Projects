ee9c8cbb848a660ac97e554b1cbbecb02929f878
Sentriarc <tran.christopherd1@gmail.com>
2024-09-15 18:46:51 -0500

    completed game (with some minor bugs)

--- P1/script.js
+++ P1/script.js
@@ -8,18 +8,25 @@ const randomChoiceBOT = Math.floor(Math.random() * BOT.length + 1);
 
 function playGame() {
     let playerInput = prompt("Rock, paper, or scissors?");
+    let UPPER_CASE = playerInput.toUpperCase();
 
     console.log(playerInput);
     // console.log(BOT);
 
-    if (randomChoiceBOT === 1) {
-        console.log("Computer chose rock.");
+    if ((randomChoiceBOT === ROCK && UPPER_CASE === "ROCK") || (randomChoiceBOT === PAPER && UPPER_CASE === "PAPER") || (randomChoiceBOT === SCISSORS && UPPER_CASE === "SCISSORS")) {
+        // console.log(`The computer chose ${randomChoiceBOT}.`); // currently prints the number rather than the actual value :(
+        console.log("It's a tie!");
     }
-    else if (randomChoiceBOT === 2) {
-        console.log("Computer chose paper.");
+    else if ((randomChoiceBOT === ROCK && UPPER_CASE === "PAPER") || (randomChoiceBOT === PAPER && UPPER_CASE === "SCISSORS") || (randomChoiceBOT === SCISSORS && UPPER_CASE === "ROCK")) {
+        console.log(`The computer chose ${randomChoiceBOT}.`); // currently prints the number rather than the actual value :(
+        console.log("The player wins!");
     }
-    else if (randomChoiceBOT === 3) {
-        console.log("Computer chose scissors.");
+    else if ((randomChoiceBOT === ROCK && UPPER_CASE === "SCISSORS") || (randomChoiceBOT === PAPER && UPPER_CASE === "ROCK") || (randomChoiceBOT === SCISSORS && UPPER_CASE === "ROCK")) {
+        console.log(`The computer chose ${randomChoiceBOT}.`); // currently prints the number rather than the actual value :(
+        console.log("The computer wins!");
+    }
+    else {
+        console.log("Something went wrong. Please try again!");
     }
 }
 
