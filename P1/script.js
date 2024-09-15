7e41e5ba6d954e85e9882e8b9d175b37722bd20b
Sentriarc <tran.christopherd1@gmail.com>
2024-09-15 18:34:15 -0500

    cleaned up code (still doesn't work properly

--- P1/script.js
+++ P1/script.js
@@ -1,41 +1,28 @@
 let ROCK = 1;
 let PAPER = 2;
 let SCISSORS = 3;
-// const BOT = Math.floor(Math.random() * 3) + 1;
+
 const BOT = [ROCK, PAPER, SCISSORS];
 const randomChoiceBOT = Math.floor(Math.random() * BOT.length + 1);
 // console.log(randomChoiceBOT)
 
-if (randomChoiceBOT === 1) {
-    console.log("Computer chose rock.");
-}
-else if (randomChoiceBOT === 2) {
-    console.log("Computer chose paper.");
-}
-else if (randomChoiceBOT === 3) {
-    console.log("Computer chose scissors.");
-}
-
 function playGame() {
     let playerInput = prompt("Rock, paper, or scissors?");
-    let LOWER_CASE = playerInput.toLowerCase();
-    let choice;
 
-    console.log(LOWER_CASE);
+    console.log(playerInput);
     // console.log(BOT);
 
-    if (randomChoiceBOT === 1 && playerInput === 1) {
-        console.log("It's a tie!");
+    if (randomChoiceBOT === 1) {
+        console.log("Computer chose rock.");
+    }
+    else if (randomChoiceBOT === 2) {
+        console.log("Computer chose paper.");
+    }
+    else if (randomChoiceBOT === 3) {
+        console.log("Computer chose scissors.");
     }
 }
 
-// function startGame() {
-//     playGame();
 
-    
-
-// }
-
-// startGame();
 
 playGame();
