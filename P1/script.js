0dbf5ceaa96bb450505e98a154b7c4f6f67e0a66
Sentriarc <tran.christopherd1@gmail.com>
2024-09-15 16:40:29 -0500

    changed functions

--- P1/script.js
+++ P1/script.js
@@ -1,24 +1,41 @@
 let ROCK = 1;
 let PAPER = 2;
 let SCISSORS = 3;
+// const BOT = Math.floor(Math.random() * 3) + 1;
+const BOT = [ROCK, PAPER, SCISSORS];
+const randomChoiceBOT = Math.floor(Math.random() * BOT.length + 1);
+// console.log(randomChoiceBOT)
 
-function generateRandomNumber() {
-    const BOT = Math.floor(Math.random() * 3) + 1;
-
-    return BOT;
-    console.log(BOT);
+if (randomChoiceBOT === 1) {
+    console.log("Computer chose rock.");
+}
+else if (randomChoiceBOT === 2) {
+    console.log("Computer chose paper.");
+}
+else if (randomChoiceBOT === 3) {
+    console.log("Computer chose scissors.");
 }
 
 function playGame() {
     let playerInput = prompt("Rock, paper, or scissors?");
-    let UPPER_CASE = playerInput.toUpperCase();
+    let LOWER_CASE = playerInput.toLowerCase();
+    let choice;
 
-    console.log(UPPER_CASE);
+    console.log(LOWER_CASE);
+    // console.log(BOT);
+
+    if (randomChoiceBOT === 1 && playerInput === 1) {
+        console.log("It's a tie!");
+    }
 }
 
-function startGame() {
-    playGame();
+// function startGame() {
+//     playGame();
 
-}
+    
 
-startGame();
+// }
+
+// startGame();
+
+playGame();
