038948f4c24d7977901ef4dae5ff89fce46ec17b
Sentriarc <tran.christopherd1@gmail.com>
2024-09-28 20:07:09 -0500

    adding a challenging feature (x button)

--- P2/script.js
+++ P2/script.js
@@ -1,9 +1,22 @@
-const hamBtn = document.querySelector('.ham-btn').addEventListener('click', openMenu);
-const menu = document.querySelector('.menu');
+document.addEventListener('DOMContentLoaded', () => {
+    const hamBtn = document.querySelector('.ham-btn');
+    const menu = document.querySelector('.menu');
+    const xBtn = document.querySelector('.x-button');
 
-function openMenu() {
-    
-}
+    menu.style.display = 'none';
+    xBtn.style.display = 'none';
 
+    hamBtn.addEventListener('click', () => {
+        if (menu.style.display === 'none') {
+            hamBtn.style.display = 'none';
+            menu.style.display = 'block';
+            xBtn.style.display = 'block';
+        }
+    });
 
-
+    xBtn.addEventListener('click', () => {
+        if (xBtn.style.display === 'block') {
+            menu.style.display === 'none';
+        }
+    });
+});
